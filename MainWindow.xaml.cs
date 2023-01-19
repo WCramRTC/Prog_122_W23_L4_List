150dfc4693d4d5666034a023b82a54747f7a4663
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-01-18 20:41:31 -0800

    Updated Notes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,211 @@ namespace Prog_122_W23_L4_List
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] firstNames = { "Will", "Hannah", "Anne" , "Will" };
+        string[] lastNames = { "Cram", "Angel", "Nguyen", "Cram" };
+        int[] grades = { 57, 101, 110, 57 };
+
+        // Keyword List<T>
+        // T = Generic
+        List<string> firstNamesList = new List<string> { "Will", "Hannah", "Anne", "Will" };
+        List<string> lastNamesList;
+        List<int> gradesList;
+
+        List<StackPanel> panels = new List<StackPanel>();
+
+        string[] newFirstNames;
         public MainWindow()
         {
             InitializeComponent();
+            lastNamesList = lastNames.ToList();
+            gradesList = grades.ToList();
+
+            panels.Add(spAddStudent);
+            panels.Add(spInsert);
+            panels.Add(spRemoveStudent);
+            panels.Add(spRemoveAt);
+
+
+            HidePanels();
+
+            panels[0].Visibility = Visibility.Visible;
+
+            DisplayFromList();
+  
+        } // MainWindow
+
+        /// <summary>
+        /// Hides all Panels
+        /// </summary>
+        public void HidePanels()
+        {
+            for (int i = 0; i < panels.Count; i++)
+            {
+                panels[i].Visibility = Visibility.Hidden;
+
+            }
+        } 
+
+        public void DisplayFromList()
+        {
+            runDisplay.Text = "";
+
+            for (int i = 0; i < firstNamesList.Count; i++)
+            {
+
+                FormatString(i);
+                
+            }
         }
-    }
-}
+
+        public void FormatString(int i)
+        {
+            //string fName = firstNamesList[i];
+            //string lName = lastNamesList[i];
+            //string grade = gradesList[i];
+
+
+            runDisplay.Text += $"{i} - {firstNamesList[i]} {lastNamesList[i]} - {gradesList[i]} \n";
+        }
+
+        private void btnAddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string fName = txtFirstName.Text;
+            string lName = txtLastName.Text;
+            int grade = int.Parse(txtGrade.Text);
+
+            // To add to a list
+
+            firstNamesList.Add(fName);
+            lastNamesList.Add(lName);
+            gradesList.Add(grade);
+
+            DisplayFromList();
+
+        } // btnAddStudent_Click
+
+        public void AnnoyingLargerArrayExample()
+        {
+            runDisplay.Text = firstNames.Length + "\n";
+
+            newFirstNames = new string[firstNames.Length * 3];
+
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+                newFirstNames[i] = firstNames[i];
+            }
+
+            firstNames = newFirstNames;
+
+            runDisplay.Text += firstNames.Length + "";
+        }
+        public void DisplayStudents()
+        {
+
+            runDisplay.Text = "";
+            for (int i = 0; i < firstNames.Length; i++)
+            {
+
+                
+
+                // First Name Last Name - Grade
+                runDisplay.Text += $"{firstNames[i]} {lastNames[i]} - {grades[i]}\n";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string fName = txtFirstName.Text;
+            string lName = txtLastName.Text;
+            int grade = int.Parse(txtGrade.Text);
+            int index = int.Parse(txtInsertAt.Text);
+
+            // To add to a list
+
+            firstNamesList.Insert(index, fName);
+            lastNamesList.Insert(index, lName);
+            gradesList.Insert(index, grade);
+
+            DisplayFromList();
+        }
+
+        private void btnRemoveStudent_Click(object sender, RoutedEventArgs e)
+        {
+            string fName = txtRemoveName.Text;
+
+            //bool wasRemoved = firstNamesList.Remove(fName);
+            
+
+            while(firstNamesList.Contains(fName))
+            {
+                firstNamesList.Remove(fName);
+            }
+
+
+            //if (wasRemoved)
+            //{
+            //    MessageBox.Show(fName + " was removed from the list");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("That name was not on the list");
+
+            //}
+            runDisplay.Text = "";
+            foreach (string name in firstNamesList)
+            {
+                runDisplay.Text += name + "\n";
+            }
+        }
+
+        private void btnRemoveAt_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(txtRemoveAt.Text);
+
+
+            if(index < firstNamesList.Count)
+            {
+                firstNamesList.RemoveAt(index);
+                lastNamesList.RemoveAt(index);
+                gradesList.RemoveAt(index);
+            }
+
+  
+
+            DisplayFromList();
+
+
+        }
+
+        private void btnRemovePanel_Click(object sender, RoutedEventArgs e)
+        {
+            HidePanels();
+            panels[0].Visibility = Visibility.Visible;
+
+        }
+
+        private void btnRemoveAtPanel_Click(object sender, RoutedEventArgs e)
+        {
+            HidePanels();
+
+            panels[1].Visibility = Visibility.Visible;
+        }
+
+        private void btnAddStudentPanel_Click(object sender, RoutedEventArgs e)
+        {
+            HidePanels();
+
+            panels[2].Visibility = Visibility.Visible;
+
+        }
+
+        private void btnInsertPanel_Click(object sender, RoutedEventArgs e)
+        {
+            HidePanels();
+
+            panels[3].Visibility = Visibility.Visible;
+
+        }
+    } // class
+
+} // namespace
